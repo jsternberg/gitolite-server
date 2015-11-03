08e31b6d45d121d666cdd2b13107500f5153d62d
Jonathan A. Sternberg <jonathansternberg@gmail.com>
2015-11-02 19:58:50 -0500

    Use config object for gitolite

--- gitolite.go
+++ gitolite.go
@@ -2,15 +2,40 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
+	"os/user"
+	"path/filepath"
 
 	"github.com/jsternberg/gitolite"
+	"golang.org/x/crypto/ssh"
 )
 
-func realMain() int {
-	config := gitolite.DefaultConfig()
-	server := gitolite.New(config)
+func loadPrivateKeyFromFile() (ssh.Signer, error) {
+	user, err := user.Current()
+	if err != nil {
+		return nil, err
+	}
+	privateKeyPath := filepath.Join(user.HomeDir, ".ssh/id_rsa")
 
+	pemBytes, err := ioutil.ReadFile(privateKeyPath)
+	if err != nil {
+		return nil, fmt.Errorf("unable to load server private key: %s", err)
+	}
+	return ssh.ParsePrivateKey(pemBytes)
+}
+
+func realMain() int {
+	privateKey, err := loadPrivateKeyFromFile()
+	if err != nil {
+		fmt.Println(err)
+		return 1
+	}
+
+	config := gitolite.DefaultConfig()
+	config.AddHostKey(privateKey)
+
+	server := gitolite.New(config)
 	if err := server.ListenAndServe(":1997"); err != nil {
 		fmt.Println(err)
 		return 1
